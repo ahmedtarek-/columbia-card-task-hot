1d0c68d057d7125490955bf83ed3d28bc64fd0a9
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2023-10-10 18:32:09 +0200

    Translate nav buttons

--- js/jspsych/poldrack_plugins/jspsych-poldrack-instructions.js
+++ js/jspsych/poldrack_plugins/jspsych-poldrack-instructions.js
@@ -41,20 +41,20 @@ jsPsych.plugins['poldrack-instructions'] = (function() {
 
         var nav_html = "<div class='jspsych-instructions-nav'>";
         if (trial.pages.length == 1) {
-          nav_html += "<button id='jspsych-instructions-next'>End Instructions</button>"
+          nav_html += "<button id='jspsych-instructions-next'>Instruktionen abschließen</button>"
         } else {
           if (current_page == 0) {
-            nav_html += "<button id='jspsych-instructions-next'>Next</button>"
+            nav_html += "<button id='jspsych-instructions-next'>Weiter</button>"
           } else if (current_page == trial.pages.length - 1) {
             if (trial.allow_backward) {
-              nav_html += "<button id='jspsych-instructions-back'>Previous</button>";
+              nav_html += "<button id='jspsych-instructions-back'>Zurück</button>";
             }
-            nav_html += "<button id='jspsych-instructions-next'>End Instructions</button>"
+            nav_html += "<button id='jspsych-instructions-next'>Instruktionen abschließen</button>"
           } else {
             if (trial.allow_backward) {
-              nav_html += "<button id='jspsych-instructions-back'>Previous</button>";
+              nav_html += "<button id='jspsych-instructions-back'>Zurück</button>";
             }
-            nav_html += "<button id='jspsych-instructions-next'>Next</button>"
+            nav_html += "<button id='jspsych-instructions-next'>Weiter</button>"
           }
         }
         nav_html += "</div>"
